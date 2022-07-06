d1e25328e7c1985e122c01fea9dbc2379f536b4d
RACHMAN21 <rachmanaldiansyah03@gmail.com>
2022-07-06 19:11:39 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function toggle() {
+  let sec = document.getElementById("sec");
+  let nav = document.getElementById("navigation");
+
+  sec.classList.toggle("active");
+  nav.classList.toggle("active");
+}
